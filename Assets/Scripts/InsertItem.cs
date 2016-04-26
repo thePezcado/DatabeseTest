2801422e7c61541663cd61a9b5bb6028f61e172d
tangkwa <tangkwa_4ever@hotmail.com>
2016-04-26 15:40:20 +0700

    DataTest

---
+++ Assets/Scripts/InsertItem.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.SqliteClient;
+using System.Data;
+using System;
+using UnityEngine.UI;
+
+public class InsertItem : MonoBehaviour {
+
+	public InputField nameInput;
+	public InputField passwordInput;
+    public InputField age;
+
+    public void Insert(){
+		string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(connectionString);
+
+		dbconn.Open(); //Open connection to the database.
+
+		IDbCommand dbcmd = dbconn.CreateCommand();
+
+		string sqlQuery = "insert into Account (userName,password,age) values ('"+ nameInput.text +"','"+ passwordInput.text + "'," + age.text + ")";
+		dbcmd.CommandText = sqlQuery;
+
+		IDataReader reader = dbcmd.ExecuteReader();
+
+		reader.Close();reader = null;
+		dbcmd.Dispose();dbcmd = null;
+		dbconn.Close();dbconn = null;
+	}
+
+    public Text highscore;
+    public void Select()
+    {
+       
+        string connectionString = "URI=file:" + Application.dataPath + "/myDatabase.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(connectionString);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "SELECT*FROM Account";
+
+
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            string Name = reader.GetString(1);
+            string Password = reader.GetString(2);
+            int age = reader.GetInt32(3);
+
+            highscore.text += Name + "-" + Password + "-" + age + "\n";
+        }
+            
+        reader.Close(); reader = null;
+        dbcmd.Dispose(); dbcmd = null;
+        dbconn.Close(); dbconn = null;
+    }
+}
